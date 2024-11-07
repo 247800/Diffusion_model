5ecd9a18eb34814ef04466b05538d6c6282e942b
247800 <114662833+247800@users.noreply.github.com>
2024-11-07 10:17:34 +0100

    Add files via upload

---
+++ train.py
@@ -0,0 +1,24 @@
+import torch
+import torchaudio
+import torch.optim as optim
+from torch import nn
+from torch.utils.data import Dataset, DataLoader
+from data_load import Audio_Dataset
+from model import Denoiser
+
+device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+print("using", device)
+
+path = "dataset"
+model_train = Denoiser()
+
+loss_func = nn.MSELoss()
+
+learning_rate = 0.001
+optimizer = optim.Adam(model_train.parameters(), lr=learning_rate)
+
+def train_loop():
+    train_loss = 0
+    loss = loss_func()
+    train_loss += loss.item()
+    return train_loss / len(dataset)
