65d73b0cd13fb0c62a3fc77a4bbbe6e9a9365090
247800 <114662833+247800@users.noreply.github.com>
2024-11-07 10:20:22 +0100

    Update train.py

--- train.py
+++ train.py
@@ -3,14 +3,14 @@ import torchaudio
 import torch.optim as optim
 from torch import nn
 from torch.utils.data import Dataset, DataLoader
-from data_load import Audio_Dataset
+from data_load import AudioDataset
 from model import Denoiser
 
 device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 print("using", device)
 
 path = "dataset"
-dataset = Audio_Dataset(path)
+dataset = AudioDataset(path)
 model_train = Denoiser()
 
 loss_func = nn.MSELoss()
