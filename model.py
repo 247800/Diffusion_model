5b4d1bb427b5022d2a448c65707f968b923649e9
247800 <114662833+247800@users.noreply.github.com>
2025-02-26 09:25:46 +0100

    Update model.py

--- model.py
+++ model.py
@@ -5,17 +5,17 @@ class Denoiser(nn.Module):
     def __init__(self):
         super(Denoiser, self).__init__()
         self.encoder = nn.Sequential(
-            nn.Conv2d(2, 64, kernel_size=3, stride=1, padding=1),
+            nn.Conv2d(1, 64, kernel_size=3, stride=1, padding=1),
             nn.ReLU(),
         )
-        # self.bottleneck = nn.Conv2d(64, 64, kernel_size=3, stride=1, padding=1)
+        self.bottleneck = nn.Conv2d(64, 64, kernel_size=3, stride=1, padding=1)
         self.decoder = nn.Sequential(
-            nn.ConvTranspose2d(64, 2, kernel_size=3, stride=1, padding=1),
+            nn.ConvTranspose2d(64, 1, kernel_size=3, stride=1, padding=1),
             # nn.ReLU()
         )
 
     def forward(self,x):
         x = self.encoder(x)
-        # x = self.bottleneck(x)
+        x = self.bottleneck(x)
         x = self.decoder(x)
         return x
