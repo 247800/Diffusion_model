cd604e34094ee894a63c93de100c41d402f0dfa6
247800 <114662833+247800@users.noreply.github.com>
2025-03-02 20:49:49 +0100

    Update sampler.py

--- sampler.py
+++ sampler.py
@@ -7,6 +7,14 @@ from torch.utils.data import Dataset, DataLoader
 from dataset import AudioDataset
 from model import Denoiser
 import torchvision.models as models
+import auraloss
+
+def get_time_schedule(sigma_min=1e-5, sigma_max=12, T=50, rho=10):
+    i = torch.arange(0, T + 1)
+    N = torch.randn(1)
+    sigma_i = (sigma_max ** (1/rho) + i * (sigma_min ** (1/rho) - sigma_max ** (1/rho)) / (N - 1)) ** rho
+    sigma_i[-1] = 0
+    return sigma_i
 
 device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 print("using", device)
@@ -14,11 +22,15 @@ print("using", device)
 path = "GTZAN_dataset"
 dataset = AudioDataset(path, train=False)
 dataloader = DataLoader(dataset, batch_size=4, shuffle=False)
-model = torch.load('denoiser.pth', weights_only=False)
+model = Denoiser()
+model.load_state_dict(torch.load('denoiser.pt', weights_only=True))
 
-# metriky
-
+loss_func = auraloss.freq.MultiResolutionSTFTLoss()
+test_loss = []
 n_steps = 10
+
+S_noise = 1
+t_i = get_time_schedule()
 
 model.eval()
 
