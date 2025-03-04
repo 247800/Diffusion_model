33b34f09741002f3e815ba270429fc393ee8bded
247800 <114662833+247800@users.noreply.github.com>
2025-03-04 18:44:11 +0100

    Update sampler.py

--- sampler.py
+++ sampler.py
@@ -11,7 +11,7 @@ import auraloss
 
 def get_time_schedule(sigma_min=1e-5, sigma_max=12, T=50, rho=10):
     i = torch.arange(0, T + 1)
-    N = torch.randn(1)
+    # N = torch.randn(1)
     sigma_i = (sigma_max ** (1/rho) + i * (sigma_min ** (1/rho) - sigma_max ** (1/rho)) / (T - 1)) ** rho
     sigma_i[T] = 0
     return sigma_i
