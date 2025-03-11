4feb5336cdd0db234db2d2ee9300fbec4adfe953
247800 <114662833+247800@users.noreply.github.com>
2025-03-11 17:24:37 +0100

    Update sampler.py

--- sampler.py
+++ sampler.py
@@ -8,56 +8,40 @@ from dataset import AudioDataset
 from model import Denoiser
 import torchvision.models as models
 import auraloss
-
-def get_time_schedule(sigma_min=1e-5, sigma_max=12, T=50, rho=10):
-    i = torch.arange(0, T + 1)
-    # N = torch.randn(1)
-    sigma_i = (sigma_max ** (1/rho) + i * (sigma_min ** (1/rho) - sigma_max ** (1/rho)) / (T - 1)) ** rho
-    sigma_i[T] = 0
-    return sigma_i
-
-def get_noise(t, S_tmin=1e-5, S_tmax=12, S_churn=None, idx=0):
-    if S_churn is None:
-        S_churn = t[idx:idx+1]
-    if S_churn < S_tmin:
-        gamma_i = 0
-    elif S_churn > S_tmax:
-        gamma_i = 0
-    else:
-        N = torch.randn(1)
-        gamma_i = min(S_churn/N, np.sqrt(1) - 1)
-    return gamma_i
+import utils.sampling_utils as s_utils
 
 device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 print("using", device)
 
 path = "GTZAN_dataset"
 dataset = AudioDataset(path, train=False)
-dataloader = DataLoader(dataset, batch_size=4, shuffle=False)
+dataloader = DataLoader(dataset, batch_size=1, shuffle=False)
 model = Denoiser()
 model.load_state_dict(torch.load('denoiser.pt', weights_only=True))
 
 loss_func = auraloss.freq.MultiResolutionSTFTLoss()
 test_loss = []
-n_steps = 10
+n_steps = 50
 
 S_noise = 1
-t = get_time_schedule()
+t = s_utils.get_time_schedule()
 
 model.eval()
+input_sig = next(iter(dataloader))
+x, _ = input_sig
+x_0 = x + torch.randn(x.shape)
 
 for step in range(n_steps):
-    for index, input_sig in enumerate(dataloader):
+    # for index, input_sig in enumerate(dataloader):
         with torch.no_grad():
-            x, _ = input_sig
-            x_0 = x + torch.randn(x.shape)
-            
-            gamma = get_noise(t=t, idx=index)
-            t_hat = t + t * gamma
-            
-            # corrupted_sig = waveform + noise * torch.randn(waveform.shape)
-            # denoised_sig = corrupted_sig.squeeze(0).squeeze(0)
-            # sample = model(denoised_sig.unsqueeze(0).unsqueeze(0))
+            gamma = s_utils.get_noise(t=t, idx=step)
+            t_hat = t[step] + t[step] * gamma
+            noise = torch.randn(x.shape)
+            x_hat = x + torch.sqrt(t_hat ** 2 - t[step] ** 2) * noise
+            D_theta = model(x_hat)
+            d = (x_hat - D_theta) / t_hat
+            x = x_hat + (t[step + 1] - t_hat) * d
+            print("step", step)
 
             # test_loss.append(loss.item())
 
